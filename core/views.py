7e9b85ebfd360bd2958aca38f895558bfd6baacb
Olga Pykhova <opy@exante.eu>
2023-04-24 19:24:48 +0300

    utils_pg - add middleware - add pagination-class - add formatter view-sets

--- core/views.py
+++ core/views.py
@@ -1,16 +1,41 @@
-from django.shortcuts import render
-from .serializers import *
+from django.http import JsonResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 
+from .serializers import *
+from .utils.util import DefaultViewSetPagination
 
-# Create your views here.
+
+class BaseViewSet(ModelViewSet):
+    pagination_class = DefaultViewSetPagination
+    filter_backends = [DjangoFilterBackend]
+    http_method_names = ['get', 'post']
+
+    def list(self, request: Request, *args, **kwargs) -> JsonResponse:
+        """Overrated method with default pagination, limit, order"""
+        query_params = self.request.query_params
+        page = int(query_params.get('page')) if query_params.get('page') else 0
+        limit = int(query_params.get('limit')) if query_params.get('limit') else 50
+        order_by = query_params.get('order_by') or 'id'
+        order = query_params.get('order') or 'asc'
+        order_str = f'-{order_by}' if order == 'desc' else order_by
+
+        queryset = self.filter_queryset(self.get_queryset()).order_by(order_str)
+        total = queryset.count()
+        serialized = self.get_serializer(queryset[page * limit:page * limit + limit], many=True)
+        return JsonResponse({
+            'pagination': {'limit': limit, 'page': page, 'total': total},
+            'content': serialized.data
+        })
+
 
 class SourceViewSet(ModelViewSet):
     queryset = Source.objects.order_by('pk')
     serializer_class = SourceSerializer
 
 
-class StatusViewSet(ModelViewSet):
+class StatusViewSet(BaseViewSet):
     queryset = Status.objects.order_by('pk')
     serializer_class = StatusSerializer
 
